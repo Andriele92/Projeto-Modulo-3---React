97b49d6f23b97a1861dd895f6bf95dde9b79fe42
Andriele92 <114115205+Andriele92@users.noreply.github.com>
2023-01-21 12:18:19 -0300

    Adicionando os primeiros arquivos do projeto

---
+++ components/Rodape.jsx
@@ -0,0 +1,14 @@
+function Rodape ()
+{
+    return (
+        <div>
+            Contato: 2140028922
+            E-mail: reactlivraria@reactlivraria.com
+            Instagram: reactlivraria
+            Endereço: Rua Ewbank da Câmara, 91 - Madureira, Rio de Janeiro - RJ, 21310-150
+            Horário de Funcionamento: 9:00 ás 20:00
+            </div> 
+    )
+}
+
+export default Rodape
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import  '../Pages/Home'
+import '../components/Rodape'
+export default App
