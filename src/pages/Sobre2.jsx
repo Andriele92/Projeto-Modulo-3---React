9ed97183368a9f3d734396acb001a088bb87e9c7
luanamiguel <114195998+luanamiguel@users.noreply.github.com>
2023-01-30 10:33:51 -0300

    Add files via upload

---
+++ src/pages/Sobre2.jsx
@@ -0,0 +1,212 @@
+import "../styles/Sobre2.css";
+
+function Sobre2() {
+  return (
+    <div>
+      <div class="container">
+        <h1 class="page-title text-center">
+          Equipe<b></b>
+        </h1>
+        <div class="row justify-content-center">
+          <div class="col-md-3 col-sm-4">
+            <div class="card">
+              <div class="card_cover"></div>
+              <div class="card_padding">
+                <div class="card_image">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/4202/4202832.png"
+                    alt=""
+                  />
+                </div>
+                <div class="card_details">
+                  <h3>Andriele</h3>
+                  <p class="text">Co-Facilitadora</p>
+                </div>
+                <div class="card_social">
+                  <a href="">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/49/49408.png"
+                      alt="Linkedin"
+                    />
+                  </a>
+                  <a href="https://github.com/Andriele92">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                      alt="Github"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-4">
+            <div class="card">
+              <div class="card_cover"></div>
+              <div class="card_padding">
+                <div class="card_image">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/4202/4202847.png"
+                    alt=""
+                  />
+                </div>
+                <div class="card_details">
+                  <h3>Rogerio</h3>
+                  <p class="text">
+                    Gestor do <br></br>Conhecimento
+                  </p>
+                </div>
+                <div class="card_social">
+                  <a href="">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/49/49408.png"
+                      alt="Linkedin"
+                    />
+                  </a>
+                  <a href="https://www.github.com/juniorrogeer">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                      alt="Github"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-4">
+            <div class="card">
+              <div class="card_cover"></div>
+              <div class="card_padding">
+                <div class="card_image">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/4202/4202843.png"
+                    alt=""
+                  />
+                </div>
+                <div class="card_details">
+                  <h3>Enrick</h3>
+                  <p class="text">
+                    Gestor de gente <br></br> e engajamento
+                  </p>
+                </div>
+                <div class="card_social">
+                  <a href="">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/49/49408.png"
+                      alt="Linkedin"
+                    />
+                  </a>
+                  <a href="https://www.github.com/EnrickM">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                      alt="Github"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-md-3 col-sm-4">
+            <div class="card">
+              <div class="card_cover"></div>
+              <div class="card_padding">
+                <div class="card_image">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/4202/4202848.png"
+                    alt=""
+                  />
+                </div>
+                <div class="card_details">
+                  <h3>Luana</h3>
+                  <p class="text">Colaborador I</p>
+                </div>
+                <div class="card_social">
+                  <a href="">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/49/49408.png"
+                      alt="Linkedin"
+                    />
+                  </a>
+                  <a href="https://github.com/luanamiguel">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                      alt="Github"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-4">
+            <div class="card">
+              <div class="card_cover"></div>
+              <div class="card_padding">
+                <div class="card_image">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/4202/4202835.png"
+                    alt=""
+                  />
+                </div>
+                <div class="card_details">
+                  <h3>David</h3>
+                  <p class="text">Colaborador II</p>
+                </div>
+                <div class="card_social">
+                  <a href="">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/49/49408.png"
+                      alt="Linkedin"
+                    />
+                  </a>
+                  <a href="https://www.github.com/DavidSCoelho">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                      alt="Github"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-4">
+            <div class="card">
+              <div class="card_cover"></div>
+              <div class="card_padding">
+                <div class="card_image">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/4202/4202837.png"
+                    alt=""
+                  />
+                </div>
+                <div class="card_details">
+                  <h3>Sergio</h3>
+                  <p class="text">Colaborador</p>
+                </div>
+                <div class="card_social">
+                  <a href="">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/49/49408.png"
+                      alt="Linkedin"
+                    />
+                  </a>
+                  <a href="">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                      alt="Github"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Sobre2;
